4eb3b312053d620c5443b4fad225cbe0ece2b8b6
Chad Ramos <jedi_obi_wan_@hotmail.com>
2017-06-09 12:26:38 -0500

    Add sass build

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,11 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var cleanCss = require('gulp-clean-css');
 
+var sassPaths = [
+  'node_modules/normalize.scss/sass',
+  'node_modules/foundation-sites/scss'
+];
+
 function clean() {
   return del([
     '_temp/**',
@@ -14,22 +19,21 @@ function clean() {
 }
 
 function styles() {
-  return gulp.src(['assets/*'])
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions', 'ie >= 9']
-    }))
-    .pipe(cleanCss({ keepSpecialComments: 0 }))
-    .pipe(gulp.dest('./app/pbi/deps'));
+  return gulp.src(['./sass/styles.scss'])
+    .pipe(sass({
+      includePaths: sassPaths,
+      outputStyle: 'compressed'
+    }).on('error', sass.logError))
+    // .pipe(autoprefixer({
+    //   browsers: ['last 2 versions', 'ie >= 9']
+    // }))
+    // .pipe(cleanCss({ keepSpecialComments: 0 }))
+    .pipe(gulp.dest('./src'));
 }
 
 function watch() {
   gulp.watch([
-    "./dev/styles/**/*.scss",
-    "./dev/widgets/pbi**/*.scss",
-    "./dev/deps/ui/**/*.scss",
-    "./dev/deps/extensions/**/*.scss",
-    "./dev/deps/directives/**/*.scss"
+    "./sass/**/*.scss"
   ], styles);
 }
 

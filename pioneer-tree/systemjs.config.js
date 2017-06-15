f4989244a2e3cc18af09652ced24c4b8bc55ce6f
Chad Ramos <jedi_obi_wan_@hotmail.com>
2017-06-15 16:24:54 -0500

    Update 2017-06-15T21:24:47.563Z

--- pioneer-tree/systemjs.config.js
+++ pioneer-tree/systemjs.config.js
@@ -7,7 +7,7 @@
     baseURL: '/pioneer-tree/',
     paths: {
       // paths serve as alias
-      'npm:': 'node_modules/',
+      'npm:': 'pioneer-tree/node_modules/',
     },
     // map tells the System loader where to look for things
     map: {
@@ -25,7 +25,7 @@
       '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
 
       // other libraries
-      'rxjs':                      'npm:rxjs',
+      'rxjs': 'npm:rxjs',
       'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
     },
     // packages tells the System loader how to load when no filename and/or no extension

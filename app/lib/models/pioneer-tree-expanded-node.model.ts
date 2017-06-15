56ef6835074bce5bd4326b794506a573ea8dcfe3
Chad Ramos <jedi_obi_wan_@hotmail.com>
2017-06-15 16:12:28 -0500

    Update 2017-06-15T21:12:23.402Z

--- app/lib/models/pioneer-tree-expanded-node.model.ts
+++
@@ -1,9 +0,0 @@
-import { IPioneerTreeNode, PioneerTreeNode } from "./pioneer-tree-node.model"
-import { IPioneerTreeRepeater, PioneerTreeRepeater } from "./pioneer-tree-repeater.model"
-
-/**
- * Sets a base type of <any> nodes to allow intellisense 
- */
-export interface IPioneerTreeExpandedNode {
-    pioneerTreeNode: IPioneerTreeNode;
-}

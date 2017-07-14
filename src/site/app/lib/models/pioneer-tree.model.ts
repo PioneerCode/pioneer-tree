ecde152954c71d0820a178d693934440160729a1
Chad Ramos <jedi_obi_wan_@hotmail.com>
2017-07-14 16:07:26 -0500

    Add public path towards api

--- src/site/app/lib/models/pioneer-tree.model.ts
+++ src/site/app/lib/models/pioneer-tree.model.ts
@@ -37,20 +37,23 @@ export interface IPioneerTree {
    * Drop currentDragNode event
    */
   dropNode(dropzone: IPioneerTreeExpandedNode, dropType: string, droppedSortIndex: number): void;
+
+  /**
+   * Expand all nodes
+   */
+  expandAllNodes(): void;
+
+  /**
+   * Collapse all nodes
+   */
+  expandAllNodes(): void;
 }
 
 @Injectable()
 export class PioneerTree implements IPioneerTree {
   currentNodes: IPioneerTreeExpandedNode[];
-  configuration: PioneerTreeConfiguration;
   currentDragNode: IPioneerTreeExpandedNode | undefined;
   currentSelectedNode: IPioneerTreeExpandedNode;
-
-  /**
-   * Limits the amount of time we need to check if the user has
-   * defined a binding field of a sort index.
-   */
-  private userSortIndexPropertySet = false;
 
   constructor(
     @Inject(PioneerTreeConfiguration) private config: IPioneerTreeConfiguration,
@@ -67,7 +70,7 @@ export class PioneerTree implements IPioneerTree {
 
     for (let i = 0; i < this.currentNodes.length; i++) {
       this.currentNodes[i].pioneerTreeNode = new PioneerTreeNode(this.uidService);
-      this.currentNodes[i].pioneerTreeNode.config = this.configuration;
+      this.currentNodes[i].pioneerTreeNode.config = this.config;
       this.currentNodes[i].pioneerTreeNode.currentNode = this.currentNodes[i];
       this.currentNodes[i].pioneerTreeNode.nodesInCollection = this.currentNodes.length;
       this.currentNodes[i].pioneerTreeNode.treeRootNodes = this.currentNodes;
@@ -129,12 +132,20 @@ export class PioneerTree implements IPioneerTree {
     this.currentDragNode = undefined;
   }
 
+  expandAllNodes(): void {
+    alert('test')
+  }
+
+  collapseAllNodes(): void {
+    alert('test')
+  }
+
   /**
    * Bind public config to default config
    */
   private buildConfiguration(configuration?: IPioneerTreeConfiguration): void {
     let config = new PioneerTreeConfiguration();
-    this.configuration = Object.assign(config, configuration);
+    this.config = Object.assign(config, configuration);
   }
 
   /**
@@ -144,7 +155,7 @@ export class PioneerTree implements IPioneerTree {
   private bindNodesToInternalTracking(nodes: IPioneerTreeExpandedNode[], parent: IPioneerTreeExpandedNode): void {
     for (let i = 0; i < nodes.length; i++) {
       nodes[i].pioneerTreeNode = new PioneerTreeNode(this.uidService);
-      nodes[i].pioneerTreeNode.config = this.configuration;
+      nodes[i].pioneerTreeNode.config = this.config;
       nodes[i].pioneerTreeNode.parentNode = parent;
       nodes[i].pioneerTreeNode.previousNode = nodes[i - 1];
       nodes[i].pioneerTreeNode.currentNode = nodes[i];
@@ -162,9 +173,8 @@ export class PioneerTree implements IPioneerTree {
    * @param node Bindable node
    */
   private setSortIndex(node: IPioneerTreeExpandedNode, index: number): void {
-    if (node[this.configuration.sortPropertyName]) {
-      this.userSortIndexPropertySet = true;
-      node.pioneerTreeNode.sortIndex = node[this.configuration.sortPropertyName];
+    if (node[this.config.sortPropertyName]) {
+      node.pioneerTreeNode.sortIndex = node[this.config.sortPropertyName];
       return;
     }
 
--- src/site/app/lib/pioneer-tree.module.ts
+++ src/site/app/lib/pioneer-tree.module.ts
@@ -1,18 +1,22 @@
 ﻿import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IPioneerTreeConfiguration } from './models/pioneer-tree-configuration.model';
 import { PioneerTreeComponent } from './components/pioneer-tree/pioneer-tree.component';
 import { PioneerTreeRepeaterComponent } from './components/pioneer-tree-repeater/pioneer-tree-repeater.component';
 import { PioneerTreeNodeComponent } from './components/pioneer-tree-node/pioneer-tree-node.component';
 import { PioneerTreeCollapseComponent } from './components/pioneer-tree-collapse/pioneer-tree-collapse.component';
 import { PioneerTreeHandleComponent } from './components/pioneer-tree-handle/pioneer-tree-handle.component';
 import { PioneerTreeDropzoneDirective } from './directives/pioneer-tree-dropzone/pioneer-tree-dropzone.directive';
-import { PioneerTree } from './models/pioneer-tree.model';
+import { PioneerTree, IPioneerTree } from './models/pioneer-tree.model';
 import { PioneerTreeDropParentService } from './services/pioneer-tree-drop-parent.service';
 import { PioneerTreeConfiguration } from './models/pioneer-tree-configuration.model';
 import { PioneerTreeDropChildService } from './services/pioneer-tree-drop-child.service';
 import { PioneerTreeDropRootService } from './services/pioneer-tree-drop-root.service';
 import { PioneerTreeUidService } from './services/pioneer-tree-uid.service';
-
+export {
+  IPioneerTreeConfiguration,
+  IPioneerTree
+}
 @NgModule({
   imports: [
     CommonModule

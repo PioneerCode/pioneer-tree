8cb4c003ce5380a9b8d792475df4a4b10929ac2d
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-05-22 15:35:54 -0500

    Clear project

--- src/site/app/lib/models/pioneer-tree-expanded-node.model.ts
+++
@@ -1,8 +0,0 @@
-﻿import { PioneerTreeNode } from './pioneer-tree-node.model';
-
-/**
- * Sets a base type of <any> nodes to allow IntelliSense
- */
-export interface IPioneerTreeExpandedNode {
-  pioneerTreeNode: PioneerTreeNode;
-}

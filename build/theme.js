1cc559b7313c860eb26508d806b91fe245956532
chad-ramos <jedi_obi_wan_@hotmail.com>
2018-06-04 16:29:11 -0500

    Add meta build

--- build/theme.js
+++ build/theme.js
@@ -3,44 +3,44 @@
  */
 const sass = require('node-sass');
 const fs = require('fs');
-const outputDirectory = './dist/pcac/themes';
+const outputDirectory = './dist/pctree/themes';
 const chalk = require('chalk');
 
 sass.render({
-  file: './projects/pcac/src/lib/pcac.scss',
+  file: './projects/pctree/src/pioneer-tree.scss',
   sourceMap: true,
-  outFile: 'pcac.css'
+  outFile: 'pioneer-tree.css'
 }, function (error, result) {
   if (fs.existsSync(outputDirectory)) {
-    writeFile(result.css.toString(), outputDirectory + "/pcac.css");
-    writeFile(result.map.toString(), outputDirectory + "/pcac.css.map");
+    writeFile(result.css.toString(), outputDirectory + "/pioneer-tree.css");
+    writeFile(result.map.toString(), outputDirectory + "/pioneer-tree.css.map");
   } else {
     return fs.mkdir(outputDirectory, function (error) {
-      writeFile(result.css.toString(), outputDirectory + "/pcac.css");
-      writeFile(result.map.toString(), outputDirectory + "/pcac.css.map");
+      writeFile(result.css.toString(), outputDirectory + "/pioneer-tree.css");
+      writeFile(result.map.toString(), outputDirectory + "/pioneer-tree.css.map");
     });
   }
 });
 
 sass.render({
-  file: './projects/pcac/src/lib/pcac.scss',
+  file: './projects/pctree/src/pioneer-tree.scss',
   sourceMap: true,
-  outFile: 'pcac.min.css',
+  outFile: 'pioneer-tree.min.css',
   outputStyle: 'compressed'
 }, function (error, result) {
   if (fs.existsSync(outputDirectory)) {
-    writeFile(result.css.toString(), outputDirectory + "/pcac.min.css");
-    writeFile(result.map.toString(), outputDirectory + "/pcac.min.css.map");
+    writeFile(result.css.toString(), outputDirectory + "/pioneer-tree.min.css");
+    writeFile(result.map.toString(), outputDirectory + "/pioneer-tree.min.css.map");
   } else {
     return fs.mkdir(outputDirectory, function (error) {
-      writeFile(result.css.toString(), outputDirectory + "/pcac.min.css");
-      writeFile(result.map.toString(), outputDirectory + "/pcac.min.css.map");
+      writeFile(result.css.toString(), outputDirectory + "/pioneer-tree.min.css");
+      writeFile(result.map.toString(), outputDirectory + "/pioneer-tree.min.css.map");
     });
   }
 });
 
 function writeFile(data, dir) {
   return fs.writeFile(dir, data, function (err) {
-    console.log(chalk.cyan('Pioneer Charts: ' + dir + ' theme was saved!'));
+    console.log(chalk.cyan('Pioneer Tree: ' + dir + ' theme was saved!'));
   });
 }
